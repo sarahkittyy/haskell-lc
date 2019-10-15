a7cabdd6be4e0de3357b49cd30127f715a6f8d96
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-14 20:55:39 -0400

    fixed application parsing

--- src/Parser/Grammar.hs
+++ src/Parser/Grammar.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE InstanceSigs #-}
+
 -- | Module for defining lambda calculus grammar.
 module Parser.Grammar where
     
@@ -9,7 +11,14 @@ import Data.Char
 type Parameters = [Token]
 
 -- | Special Tokens
-data Token = Lambda Char | Period Char | Identifier Char deriving (Show)
+data Token = Lambda Char | Period Char | Identifier Char
+
+-- | For pretty printing of Tokens
+instance Show Token where
+    show :: Token -> String
+    show (Lambda c) = [c]
+    show (Period p) = [p]
+    show (Identifier s) = [s]
 
 parseToken :: Parser Token
 parseToken = parseLambda <|> parsePeriod <|> parseIdentifier
@@ -24,10 +33,21 @@ parseIdentifier :: Parser Token
 parseIdentifier = Identifier <$> satisfies isLetter
 
 -- | The main expression type defining lc grammar.
-data Expr = Literal Token | Function Parameters Expr | Application Expr Expr deriving (Show)
+data Expr = Wrapped Expr | Literal Token | Function Parameters Expr | Application Expr Expr
+
+-- | For pretty printing expressions
+instance Show Expr where
+    show :: Expr -> String
+    show (Literal t) = show t
+    show (Function params expr) = "λ" ++ (concatMap show params) ++ "." ++ (show expr)
+    show (Application lexpr rexpr) = "[" ++ (show lexpr) ++ " " ++ (show rexpr) ++ "]"
+    show (Wrapped expr) = "(" ++ (show expr) ++ ")"
 
 parseExpr :: Parser Expr
-parseExpr = parseFunction <|> parseApplication <|> parseLiteral
+parseExpr = parseApplication <|> parseFunction <|> parseLiteral <|> parseWrapped <|> failure "Invalid LC expression"
+
+parseWrapped :: Parser Expr
+parseWrapped = Wrapped <$> parens parseExpr
 
 parseLiteral :: Parser Expr
 parseLiteral = Literal <$> parseIdentifier
@@ -45,7 +65,7 @@ parseFunction = do
     
 parseApplication :: Parser Expr
 parseApplication = do
-    fn <- parseFunction <|> parseLiteral
-    char ' '
-    arg <- parseApplication <|> parseLiteral
+    fn <- parseWrapped <|> parseFunction <|> parseLiteral
+    spacing
+    arg <- parseExpr
     return $ Application fn arg
