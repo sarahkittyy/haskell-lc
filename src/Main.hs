987fa7592878b5ad4f1dc14ea3f57d713852a65b
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-14 20:56:51 -0400

    added call to parseExpr in main

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,12 @@
 module Main where
 
+import System.Environment
+import Data.List
+
+import Parser.Grammar
+import Parser.Parser
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    args <- getArgs
+    putStrLn $ show $ runParser parseExpr $ intercalate " " args
